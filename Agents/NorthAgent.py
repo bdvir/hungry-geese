af2bef5177490e820484d66601de28110758a6ba
Guy122 <56473822+Guy122@users.noreply.github.com>
2021-04-30 15:29:00 +0300

    Add files via upload

---
+++ Agents/NorthAgent.py
@@ -0,0 +1,8 @@
+def go_north_agent(obs, conf):
+    print("obs ", obs)
+    # print("conf", conf)
+    # ac = random.choice(list(Action)).name
+    # global string; aclist2 = ac
+    # Action.NORTH, Action.EAST, Action.SOUTH, Action.WEST
+    # return random.choice(list(Action)).name;  # random agent dies quickly
+    return Action.NORTH.name
